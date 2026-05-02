f03319739abab3402dc7ab8191ac8f561d07b4bb
FluctLight <andresjatar@gmail.com>
2026-05-02 10:24:04 -0400

    estructura agregado

---
+++ estructura.py
@@ -0,0 +1,18 @@
+x = 3
+if x > 0:
+    print("x es positivo")
+elif x == 0:
+    print("x es cero")    
+      
+else:
+    print("x es negativo o cero")
+    
+#estructura de control for
+for i in range(3):
+    print(f"i={i}")
+
+#estructura de control while
+n = 3
+while n > 0:
+    print(n)
+    n -= 1   
